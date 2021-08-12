7c61750bd0fea8e79f343114b0a9d9087a09adfc
pram <you@example.com>
2021-08-13 00:37:13 +0700

    updateUi

---
+++ app.js
@@ -0,0 +1,6 @@
+const navbar = document.getElementById('navbar-wrrap');
+
+navbar.addEventListener('click', function(e) {
+    const navTarget = document.getElementsByClassName('nav-link')[0];
+    
+})
